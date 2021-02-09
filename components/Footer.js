e1193291f02d427fb6700c11a4cd517a8e4237f3
plaetzaw <plaetzaw@gmail.com>
2021-02-09 14:04:37 -0600

    Set up basics

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <p>Yo this is my stuff 2021</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className='logo'>
+          <h1>Player List</h1>
+        </div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Player List</a>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
